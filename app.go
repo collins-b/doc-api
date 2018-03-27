7a88324af148e6381a3fbf8d1d9586b47a0f0256
Collins <collinsdelan@gmail.com>
2018-03-27 15:11:44 +0300

    add test file

--- app.go
+++ app.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -14,7 +16,16 @@ type App struct {
 }
 
 // Initialize db connection
-func (a *App) Initialize(user, password, dbName string) {}
+func (a *App) Initialize(user, password, dbName string) {
+	connectionString := fmt.Sprintf("%s:%s@/%s", user, password, dbName)
+
+	var err error
+	a.DB, err = sql.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	a.Router = mux.NewRouter()
+}
 
 // Run app
 func (a *App) Run(addr string) {}
